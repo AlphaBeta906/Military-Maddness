f5d4862df2ec25b4fc6443bbcf67155b3be27c1a
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-12-03 19:05:40 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,19 @@
 import javax.swing.JFrame;
 
 public class Main {
+  
+  JFrame window;
+  
   public static void main(String[], args) {
-    
+    new Game();
   }
+  
+  public Game(){
+    
+    window = new JFrame;
+    widnow.setSize(800, 600);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+  }
+  
 }
