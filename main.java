9076844e7710a15afaa3fa2d7787d43f4268c072
AlphaBeta906 <63622656+AlphaBeta906@users.noreply.github.com>
2020-12-04 09:51:10 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,24 +1,28 @@
 import java.awt.Color;
+import java.awt.Container;
+
 import javax.swing.JFrame;
 
 public class Main
 {
 
   JFrame window;
+  Container con;
 
-  public static void main (String[]args)
+  public static void main(String[]args)
   {
-    new Game ();
+    new Game();
   }
 
-  void Game (){
+  void Game(){
 
     window = new JFrame ();
-    widnow.setSize (800, 600);
-    window.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-    window.setContentPane ().setBackround (Color.black);
-    window.setLayout (null);
-    window.setVisible (true);
+    widnow.setSize(800, 600);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.getContentPane().setBackround(Color.black);
+    window.setLayout(null);
+    window.setVisible(true);
+    cons = window.getContentPane()
 
   }
 
